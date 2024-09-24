d606c0fdf702160adfa81fbefc8c6d9b228d582a
aditya chechani <ady.chechani@gmail.com>
2024-09-25 00:32:27 +0530

    Added query_processor.py

--- query_processor.py
+++ query_processor.py
@@ -6,10 +6,9 @@ from pymongo import MongoClient
 from langchain_mongodb import MongoDBAtlasVectorSearch
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_history_aware_retriever
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema import BaseMessage, HumanMessage, AIMessage
+
 
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
@@ -24,20 +23,21 @@ collection = client[db_name][collection_name]
 embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001',
                                           google_api_key=os.getenv('GOOGLE_API_KEY'),
                                           task_type='retrieval_query')
-vectorstore = MongoDBAtlasVectorSearch(collection, embeddings)
+atlas_search_index = "FinRAG-search-index"
+vectorstore = MongoDBAtlasVectorSearch(collection=collection,
+                                       embedding=embeddings,
+                                       index_name=atlas_search_index,
+                                       relevance_score_fn="cosine")
 
 class Generator:
     def __init__(self) -> None:
         self.__llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-        self.__retriever = vectorstore.as_retriever(
-            search_type="mmr",  # MMR Algorithm
-            search_kwargs={"k": 6, "fetch_k": 50}
-        )
+        self.__retriever = vectorstore.as_retriever(search_kwargs={"k": 6})
         self.__out_parser = StrOutputParser()
         # self.__chat_history = []
         # self.__summary = None
 
-    def __retrieve_documents(self, query):
+    def __retrieve_documents(self, query : str):
         return self.__retriever.invoke(query)
 
     def __generator(self, query : str):
@@ -50,9 +50,14 @@ class Generator:
 
         rag_chain = prompt | self.__llm | self.__out_parser
 
-        answer = rag_chain.invoke({"question": query, "context": docs})
+        context = "\n".join([doc.page_content for doc in docs])
+        print(context)
+        print("----")
+        print(docs)
+        answer = rag_chain.invoke({"question": query, "context": context})
         return answer
 
     def process(self, query: str):  # Call will be made to this function to generate answers
         answer = self.__generator(query)
         return answer
+
